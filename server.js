aed3adbffe89402dba47c7b4e6f5019bc8e0ac1b
ehdrma <ehdrma99@gmail.com>
2023-11-07 04:16:27 +0900

    bb

--- server.js
+++ server.js
@@ -7,11 +7,51 @@ const Article = require('./models/Article'); // Article 모델 불러오기
 const scrapeNHKEasyNews = require('./scrapy'); // 스크래핑 함수 불러오기
 const translateArticles = require('./translate'); // 번역 함수 불러오기
 
+const { TextToSpeechClient } = require('@google-cloud/text-to-speech');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const { Storage } = require('@google-cloud/storage');
+const fs = require('fs');
+const util = require('util');
+
 const app = express();
 const PORT = 5000;
 const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
+
+const keyFilename = path.join(__dirname, 'newnihon-tts.json');
+const client = new TextToSpeechClient({ keyFilename });
+
+app.post('/text-to-speech', async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    const request = {
+      input: { text },
+      voice: { languageCode: 'ja-JP', ssmlGender: 'NEUTRAL' }, // 변경 가능한 옵션
+      audioConfig: { audioEncoding: 'LINEAR16', speakingRate: 0.8 },
+    };
+
+    const [response] = await client.synthesizeSpeech(request);
+
+    const audioFilename = `${uuidv4()}.wav`; // 고유한 파일명 생성
+    const outputPath = `./public/${audioFilename}`; // 파일 저장 경로
+
+    // 음성 파일 저장
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile(outputPath, response.audioContent, 'binary');
+
+    // 클라이언트로 응답 보내기
+    res.json({ audioUrl: `http://192.168.219.107:5000/${audioFilename}` });
+  } catch (error) {
+    console.error('Error while processing text to speech:', error);
+    res.status(500).json({ error: '서버 오류입니다.' });
+  }
+});
+
+app.use(express.static('public')); // 정적 파일 제공을 위한 미들웨어 설정
 
 // MongoDB 연결
 mongoose.connect('mongodb+srv://ehdrma99:fjqmffl99~@newnihon-cluster.tu3nh1c.mongodb.net/newnihon', {
